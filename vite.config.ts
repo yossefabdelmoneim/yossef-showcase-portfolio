07a17856802adb429bc53f0ece238b9e858a793f
yossefabdelmoneim <yossefabdelmoneim9@gmail.com>
2026-02-22 22:57:35 +0200

    clean config and prepare for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/yossef-portfolio/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,10 +13,12 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
