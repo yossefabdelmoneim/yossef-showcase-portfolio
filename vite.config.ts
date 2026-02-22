fdd4a5b38e0af056842e22dc8d6f13ab6aae9844
yossefabdelmoneim <yossefabdelmoneim9@gmail.com>
2026-02-22 23:17:27 +0200

    setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yossef-portfolio/",
+  base: "/yossef-showcase-portfolio/",
 
   server: {
     host: "::",
