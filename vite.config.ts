e23308bd6d92a252830590469f4b57ea13717ce3
yossefabdelmoneim <yossefabdelmoneim9@gmail.com>
2026-02-22 23:38:37 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yossef-showcase-portfolio",
+  base: "/yossef-showcase-portfolio/",
 
   server: {
     host: "::",
