63153618985790aca242ea104408e3e9e156140c
yossefabdelmoneim <yossefabdelmoneim9@gmail.com>
2026-02-24 21:07:41 +0200

    setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-
+  base: '/yossef-showcase-portfolio/',
 
   resolve: {
     alias: {
